db1f30d10a55806cb0db74ae38f04b0146b9493b
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-04-11 04:36:58 +0700

    Update for modulus calculation

--- script.js
+++ script.js
@@ -63,6 +63,9 @@ const calculate = () => {
     case "/":
       result = prevNumber  / currentNumber;
       break;
+    case "%":
+      result = prevNumber  % currentNumber;
+      break;
     default:
       break;
   }
